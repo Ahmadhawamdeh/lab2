23a9b50ad992daacd0727bd7c8398fa3c0980f90
ahmadhawamdeh <ahmadhawamdeh313@gmail.com>
2019-10-07 15:37:49 +0300

    add

---
+++ js/app.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var name = prompt('whats your name?');
+alert('welcome  '  +name)
+
+
+var name = prompt('Do you know what my name is??');
+name = name.toLowerCase();
+if (name == 'yes') {
+    alert(" WOW!! You made me happy");
+} else if (name == 'y') {
+    alert(" WOW!! You made me happy");
+}
+else if (name == 'no') {
+    alert("Saddened me!");
+}
+
+
+
+var age = prompt('Do you know how old I am?');
+age = age.toLowerCase();
+if (age == 'yes') {
+    alert(" very good");
+} else if (age == 'y') {
+    alert(" very good");
+}
+else if (age == 'no') {
+    alert("ooh!! iam sad");
+}
+
+
+var addriss = prompt('Now tell me do you know my address?');
+Addresses = Addresses.toLowerCase();
+if (Addresses == 'yes') {
+    alert(" very good");
+} else if (Addresses == 'y') {
+    alert(" very good");
+}
+else if (Addresses == 'no') {
+    alert("i am in amman city - university street");
+}
+
+var stud = prompt('Tell me do you know what my studies are?');
+stud = stud.toLowerCase();
+if (stud == 'yes') {
+    alert(" very good");
+} else if (stud == 'y') {
+    alert(" very good");
+}
+else if (stud == 'no') {
+    alert("i am studed MIS");
+}     
+
+var sport = prompt('do you know what my fav sport ?');
+sport = sport.toLowerCase();
+if (sport == 'yes') {
+    alert("great");
+} else if (sport == 'y') {
+    alert(" great");
+}
+else if (sport == 'no') {
+    alert("i love boxing ");
+}     
+
+
+
+
+//switch (summer) {
+
+  //  case 'yes':
+     //   alert('very good');
+//break;
+
+ //   case 'y':
+       // alert('very good');
+      //  break;
+
+ //   default:
+     //   alert('why you dont')
+//}
+//if (life == 'yes') {// YES, yEs, YEs,
+  //  alert('very good');
+//} else if (life == 'y') {
+    //alert('very good');
+//}
+//else if (life == 'no') {// NO, no, No,nO
+  //  alert('im sad for you , why you dont')
+//}
+//else {
+  //  alert('why you dont?')
+//}
