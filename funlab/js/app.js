1973ceaf67dd51ff58c50319e7a44195dab76a10
ahmadhawamdeh <ahmadhawamdeh313@gmail.com>
2019-10-12 01:49:18 +0300

    edit css

---
+++ funlab/js/app.js
@@ -0,0 +1,15 @@
+// a function is like the box
+
+function makeCoffe(numberOfCups, style, withmilk) {
+    // var numberOfCups = the value of the first parametr
+    console.log('hoping to making coffe for ' + numberOfCups);
+    // return undefined;
+    return 'making ' + numberOfCups + 'cups of' + style + ' caffee ';
+    console.log('after return');
+}
+// parameter
+var coffeResults = makeCoffe(1, 'turkish');
+//var logResult = console.log('coffeResults' + coffeResults);
+console.log('coffeeresults' + coffeResults);
+
+<div id="flash_screenreader_holder" role="alert" aria-live="assertive" aria-relevant="additions" class="screenreader-only" aria-atomic="false"></div>
